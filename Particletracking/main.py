b7e822901dae02dab93a9ce03237846ea91baab6
christiangynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-10-14 22:40:35 +0200

    Remove unnecessary comments in the particle tracking scripts

--- Particletracking/main.py
+++ Particletracking/main.py
@@ -26,24 +26,7 @@ def generate_trajectories(path, number_of_frames=-1):
 
 def filter_trajectories(trajectories):           
     trajectories = tp.filter_stubs(trajectories, 5)
-    #Out commented code migth be usefull for the rapport could be put into an actuall function or and if statmwnt if plots are desired
-    # print('Before:', trajectories['particle'].nunique())
-    # print('After:', trajectories_1['particle'].nunique())
-    # plt.figure()
-    # tp.mass_size(trajectories_1.groupby('particle').mean())
-    # tp.mass_size(trajectories_2.groupby('particle').mean())
-    trajectories = trajectories[(trajectories['size'] > 3.3)]# & (trajectories['signal'] > 7)]
-        
-        #(trajectories['mass'] > 50) &
-    #                            (trajectories['size'] < 2.6) & 
-    #                            (trajectories['size'] > 0.3) &  
-    #                            (trajectories['ecc'] < 0.3) &
-    #                            (trajectories['signal'] > 130)
-
-    # plt.figure()
-    # tp.annotate(trajectories_2[trajectories_2['frame'] == 0], frames[0]);
-    # tp.plot_traj(trajectories_2)
-    # plt.show()
+    trajectories = trajectories[(trajectories['size'] > 3.3)]
     return trajectories
 
 def remove_drift(trajectories):
@@ -53,9 +36,7 @@ def remove_drift(trajectories):
 
 visualise = False
 
-#First time this was actually required
 if __name__ == "__main__":
-
     filepaths = ["data/white paint/A/BF-A-W-deep-fried.avi",
                  "data/white paint/A/DF-A-W-2-improved-DF-deep-fried.avi",
                  "data/white paint/A/PHC_A_W-deep-fried.avi",
@@ -91,72 +72,40 @@ if __name__ == "__main__":
 
             for i in range(10):
                 ax = tp.annotate(trajectories[trajectories["frame"] == i], frames[i], plot_style={"markersize": 2, "alpha": 0.2})
-
-            #print("Hm")
-            #figure = plt.figure()
-            #figure.add_axes(ax)
-            #figure.savefig("annotation.png")
+   
+        print("Finding mean square displacement")
+        em = tp.emsd(trajectories, 0.151285759**2, 5) # microns per pixel = 0.151285759, frames per second = 122/19.99 = 6.10
 
 
-        # tm = generate_datastructure("data/white paint/A/BF-A-W_2.avi", 120)
-        #tp.plot_traj(trajectories)
-        #plt.show()
-        #plt.figure()
-        
-        if True:
-            print("Finding mean square displacement")
-            em = tp.emsd(trajectories, 0.151285759**2, 5) # microns per pixel = 0.55., frames per second = 122/19.99
+        plt.figure()
+        plt.plot(em.index, em.values)
 
+        x = np.reshape(em.index, (len(em.index), 1))
+        regression = LinearRegression(fit_intercept=False).fit(x, em.values)
+        D = regression.coef_[0]/4
 
-            plt.figure()
-            plt.plot(em.index, em.values)
+        print(f"Results for {filepath}:")
 
-            x = np.reshape(em.index, (len(em.index), 1))
-            regression = LinearRegression(fit_intercept=False).fit(x, em.values)
-            D = regression.coef_[0]/4
+        print(f"D={round(D,4)} [(µm)^2/s]")
 
-            print(f"Results for {filepath}:")
+        plt.plot(em.index, regression.predict(x))
+        plt.savefig("plot.png")
 
-            print(f"D={round(D,4)} [(µm)^2/s]")
+        k_B = 1.38*10**-23 # [J/K]
+        T = 293 # [K]
+        viscocity = 0.001 # [Pa*s]
 
-            plt.plot(em.index, regression.predict(x))
-            plt.savefig("plot.png")
+        R = k_B*T/(6*pi*viscocity*D)*10**12*10**6
+        print(f"R:{round(R,2)} [µm]")
 
-            k_B = 1.38*10**-23 # [J/K]
-            T = 293 # [K]
-            viscocity = 0.001 # [Pa*s]
-
-            R = k_B*T/(6*pi*viscocity*D)*10**12*10**6
-            print(f"R:{round(R,2)} [µm]")
-
-            diffusion_constants.append(round(float(D), 4))
-            hydrodynamic_radiuses.append(round(float(R),2))
+        diffusion_constants.append(round(float(D), 4))
+        hydrodynamic_radiuses.append(round(float(R),2))
 
         
     print(f"diffuion constants:{diffusion_constants} [(µm)^2/s]")
     print(f"mean: {round(np.mean(diffusion_constants), 4)} [(µm)^2/s]")
     print(f"std: {round(np.std(diffusion_constants), 4)} [(µm)^2/s]")
 
-
     print(f"hydrodynamic radius:{hydrodynamic_radiuses} [(µm)]")
     print(f"mean: {round(np.mean(hydrodynamic_radiuses), 2)} [(µm)]")
     print(f"std: {round(np.std(hydrodynamic_radiuses), 2)} [(µm)]")
-    
-
-        #x,y = em.index, em[em.index]
-        #reg = LinearRegression().fit(x, y)
-
-
-        #print(f"em:{em}")
-        #em_2 = em[em.index < em.index[-1] / 1]
-        #print(len(em_2.index), len(em.index))
-        #fig, ax = plt.subplots()
-        #ax.plot(em_2.index, em_2, 'o')
-        #ax.set_xscale('log')
-        #ax.set_yscale('log')
-        #ax.set(ylabel=r'$\langle \Delta r^2 \rangle$ [$\mu$m$^2$]',
-        #   xlabel='lag time $t$')
-
-        #linear_regress = tp.utils.fit_powerlaw(em_2)  # performs linear best fit in log space, plots]
-        #print(linear_regress)
-        #print("D = ",linear_regress["A"]["msd"]/4)
